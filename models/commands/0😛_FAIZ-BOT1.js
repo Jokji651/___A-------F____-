59854f91d5067cebfce06960a6816c2b8b287372
Ariffaiz90 <payalthakur969491@gmail.com>
2024-10-27 08:39:25 +0530

    Update and rename bot.js to 0😛_FAIZ-BOT1.js

--- models/commands/0😛_FAIZ-BOT1.js
+++ models/commands/0😛_FAIZ-BOT1.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "goibot",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "PREM BABU",
+  credits: "FAIZ ANSARI",
   description: "goibot",
   commandCategory: "Noprefix",
   usages: "noprefix",
