2a913056223fab98c3c8c30b803e72dfe4a41be3
Ariffaiz90 <payalthakur969491@gmail.com>
2024-10-27 07:40:48 +0530

    Update 0out.js

--- models/commands/0out.js
+++ models/commands/0out.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "Priyansh",
+    credits: "faiz",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
