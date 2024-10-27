4dfef3b568c6a36b072649f08eb92250b9673606
Ariffaiz90 <payalthakur969491@gmail.com>
2024-10-27 07:24:41 +0530

    Update and rename 0_meme.js to 0_FAIZ-meme.js

--- models/commands/0_FAIZ-meme.js
+++ models/commands/0_FAIZ-meme.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "meme",
   version: "2.0.5",
   hasPermssion: 0,
-  credits: "PREM BABU",
+  credits: "FAIZ ANSARI",
   description: "photo",
   commandCategory: "Fun picture ",
   usages: "type meme",
